48fd13d2a17361b73b37f0affd827e7a9c36f5d4
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-02-10 17:47:37 -0600

    wip: json based API

---
+++ src/api/Benchpress_api.ml
@@ -0,0 +1,11 @@
+
+module Protocol_j = Protocol_j
+module Protocol_t = Protocol_t
+
+
+(* TODO: simple TCP server? *)
+(* TODO: simple TCP client? *)
+
+(* TODO: hooks for making it easier to plug into [Serve] with /api/ endpoint *)
+
+(* TODO: client based on curly *)
