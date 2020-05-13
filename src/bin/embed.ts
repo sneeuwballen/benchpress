c696dd47dea3e0e9e9307caf6d71d335d4d49ce3
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-05-12 21:59:13 -0400

    fix ts compilation, improve lazy loading of error/bad lists

--- src/bin/embed.ts
+++ src/bin/embed.ts
@@ -1,19 +1,34 @@
 
-function lazyLoad(e: HTMLElement, target: string) {
+async function lazyLoad(e: HTMLElement, target: string) {
     //return h('h2', null, `lazy load target=${target}`);
     console.log(`fetch ${target}`);
-    fetch(target)
-        .then(res => res.text())
-        .then(body => {
-            console.log(`got body for ${target}`);
-            e.innerHTML = body;
-        });
+    e.innerHTML = `<div class="spinner-border">
+        <span class="sr-only">loading</span>
+        </div>`;
+    const res = await fetch(target);
+    const body = await res.text();
+    console.log(`got body for ${target}`);
+    if (res.ok) {
+        e.innerHTML = body;
+    } else {
+        e.innerHTML = `<div class="alert alert-danger">
+            error ${res.status}
+            <ul class="container-list">
+                <li class="container-list-item">
+                    <p>context: lazy loading of ${target} </p>
+                </li>
+                <li class="container-list-item">
+                    <p>error: ${body} </p>
+                </li>
+            </ul>
+        </div>`;
+    }
 }
 
 function lazyLoadAll() {
     console.log('lazy load all');
     document.querySelectorAll<HTMLElement>('.lazy-load').forEach(e => {
-      let target = <string> e.getAttribute('x_src');
+      const target = <string> e.getAttribute('x_src');
       lazyLoad(e, target);
     });
 }
