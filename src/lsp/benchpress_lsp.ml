4d7ec1ca1e1c1258ee414169a970215ff290aeb9
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-11-21 23:05:15 -0500

    fix lsp

--- src/lsp/benchpress_lsp.ml
+++ src/lsp/benchpress_lsp.ml
@@ -48,6 +48,16 @@ class blsp = object(self)
   val buffers: (Lsp.Types.DocumentUri.t, processed_buf) Hashtbl.t Lock.t
     = Lock.create @@ Hashtbl.create 32
 
+  method! config_hover = Some (`Bool false)
+  method! config_definition = Some (`Bool false)
+
+  (* configure how sync happens *)
+  method! config_sync_opts =
+    let change = LT.TextDocumentSyncKind.Incremental in
+    LT.TextDocumentSyncOptions.create ~openClose:true ~change
+      ~save:(LT.SaveOptions.create ~includeText:false ())
+      ()
+
   (* We define here a helper method that will:
      - process a document
      - store the state resulting from the processing
@@ -56,34 +66,41 @@ class blsp = object(self)
   method private _on_doc
       ~(notify_back:L.notify_back)
       (uri:Lsp.Types.DocumentUri.t) (contents:string) =
+    Log.debug (fun k->k"on doc %s" uri);
     let r = Stanza.parse_string ~filename:uri contents in
     Lock.with_lock buffers (fun b -> Hashtbl.replace b uri r);
     let diags = diagnostics ~uri r in
+    Log.debug (fun k->k"send diags for %s" uri);
     notify_back#send_diagnostic diags
 
   (* We now override the [on_notify_doc_did_open] method that will be called
      by the server each time a new document is opened. *)
-  method on_notif_doc_did_open ~notify_back d ~content : unit IO.t =
+  method on_notif_doc_did_open ~notify_back (d:LT.TextDocumentItem.t) ~content : unit IO.t =
     self#_on_doc ~notify_back d.uri content
 
   (* Similarly, we also override the [on_notify_doc_did_change] method that will be called
      by the server each time a new document is opened. *)
-  method on_notif_doc_did_change ~notify_back d _c ~old_content:_old ~new_content =
+  method on_notif_doc_did_change ~notify_back
+      (d:LT.VersionedTextDocumentIdentifier.t) _c ~old_content:_old ~new_content =
     self#_on_doc ~notify_back d.uri new_content
 
   (* On document closes, we remove the state associated to the file from the global
      hashtable state, to avoid leaking memory. *)
-  method on_notif_doc_did_close ~notify_back:_ d : unit IO.t =
+  method on_notif_doc_did_close ~notify_back:_
+      (d:LT.TextDocumentIdentifier.t) : unit IO.t =
+    Log.debug (fun k->k"close %s" d.uri);
     Lock.with_lock buffers (fun b -> Hashtbl.remove b d.uri);
     IO.return ()
 end
 
 let setup_debug() =
   if Sys.getenv_opt "LSP_DEBUG"=Some "1" then (
-    let out = open_out "/tmp/lsp.log" in
-    let out = Format.formatter_of_out_channel out in
+    let oc = open_out "/tmp/lsp.log" in
+    let out = Format.formatter_of_out_channel oc in
+    at_exit (fun () -> Format.fprintf out "@?"; close_out oc);
     Logs.set_reporter (Logs.format_reporter ~app:out ~dst:out ());
     Logs.set_level ~all:true (Some Logs.Debug);
+    Log.info (fun k->k"started LSP");
   )
 
 let () =
